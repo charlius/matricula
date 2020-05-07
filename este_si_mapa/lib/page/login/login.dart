ba1c92223ff8f0b8717b92109ad8fb5951526627
charlius <charlshsx@gmail.com>
2020-05-07 10:51:50 -0400

    actualizacion

--- este_si_mapa/lib/page/login/login.dart
+++ este_si_mapa/lib/page/login/login.dart
@@ -20,7 +20,9 @@ class MyApp extends StatelessWidget {
 
 
 
-  // This widget is the root of your application.
+  // This widget is the root of your application.hola
+
+
   @override
   Widget build(BuildContext context) {
 
